bd7ed203e8ed59af1a718cf029a6e82c328be6ee
Antonio Dioquino <antonio.dioquino@hotmail.com>
2026-04-05 22:35:58 +0800

    initial project commit

---
+++ booku-api/src/main/java/com/thrddqno/booku_api/BookuPmsApplication.java
@@ -0,0 +1,13 @@
+package com.thrddqno.booku_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookuPmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookuPmsApplication.class, args);
+	}
+
+}
---
+++ booku-api/src/test/java/com/thrddqno/booku_api/BookuPmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thrddqno.booku_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookuPmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
